ccbc5ddf22c6012046a4c6a775b37ff2342956d5
Wayne Sanders <107742337+WMSANDERS85@users.noreply.github.com>
2023-08-28 09:11:07 -0700

    Add serverless authorization functions
    added the getAuthURL and getAccessToken functions

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,19 +1,68 @@
-'use strict';
+const {google} = require('googleapis');
 
-module.exports.hello = async (event) => {
+const calendar = google.calendar('v3');
+const SCOPES = [
+  'https://www.googleapis.com/auth/calendar.events.public.readonly',
+];
+const {CLIENT_SECRET, CLIENT_ID, CALENDAR_ID} = process.env;
+const redirect_uris = ['https://wmsanders85.github.io/meet/'];
+
+const oAuth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  redirect_uris[0]
+);
+
+module.exports.getAuthURL = async () => {
+  /**
+   *
+   * Scopes array is passed to the `scope` option.
+   *
+   */
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+    body: JSON.stringify({
+      authUrl,
+    }),
   };
+};
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+module.exports.getAccessToken = async (event) => {
+  // Decode authorization code extracted from the URL query
+  const code = decodeURIComponent(`${event.pathParameters.code}`);
+
+  return new Promise((resolve, reject) => {
+    oAuth2Client.getToken(code, (error, response) => {
+      if (error) {
+        return reject(error);
+      }
+      return resolve(response);
+    });
+  })
+    .then((results) => {
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credintials': 'true',
+        },
+        body: JSON.stringify(results),
+      };
+    })
+    .catch((error) => {
+      return {
+        statusCode: 500,
+        body: JSON.stringify(error),
+      };
+    });
 };
 
