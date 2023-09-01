6048e0aaacb915a66ecf5bb09e697a85635c4b5a
Wayne Sanders <107742337+WMSANDERS85@users.noreply.github.com>
2023-09-01 15:55:01 -0700

    write test cases for show/hide  event details
    test and implement show/hide event details button functionality

--- src/_test_/Event.test.js
+++ src/_test_/Event.test.js
@@ -1,6 +1,7 @@
 import {render} from '@testing-library/react';
 import Event from '../components/Event';
 import mockData from '../mock-data';
+import userEvent from '@testing-library/user-event';
 
 const mockEvent = mockData[0];
 
@@ -28,5 +29,32 @@ describe('<Event /> component', () => {
   test('show details button', () => {
     const button = EventComponent.container.querySelector(`.details-btn`);
     expect(button).toBeInTheDocument();
+  });
+
+  test('by default, event details are not shown', () => {
+    const eventDom = EventComponent.container.firstChild;
+    const details = eventDom.querySelector(`.details`);
+    expect(details).not.toBeInTheDocument();
+  });
+
+  test('click on show details button, event details are shown', async () => {
+    const user = userEvent.setup();
+    const button = EventComponent.container.querySelector(`.details-btn`);
+    await user.click(button);
+
+    const eventDom = EventComponent.container.firstChild;
+    const details = eventDom.querySelector(`.details`);
+    expect(details).toBeInTheDocument();
+  });
+
+  test('click on hide details button, event details are hidden', async () => {
+    const user = userEvent.setup();
+    const button = EventComponent.container.querySelector(`.details-btn`);
+    await user.click(button);
+    await user.click(button);
+
+    const eventDom = EventComponent.container.firstChild;
+    const details = eventDom.querySelector(`.details`);
+    expect(details).not.toBeInTheDocument();
   });
 });
--- src/components/Event.js
+++ src/components/Event.js
@@ -1,10 +1,33 @@
+import {useState} from 'react';
+
 const Event = ({event}) => {
+  const [showDetails, setShowDetails] = useState(false);
   return (
     <li>
       <h2 className="event-title">{event.summary}</h2>
       <p className="eventStart">{event.created}</p>
       <p className="event-location">{event.location}</p>
-      <button className="details-btn">Details</button>
+      <button
+        className="details-btn"
+        onClick={() => {
+          setShowDetails(!showDetails);
+        }}
+      >
+        {showDetails ? 'Hide Details' : 'Show Details'}
+      </button>
+      {showDetails ? (
+        <div className="details">
+          <h2 className="event-title">{event.summary}</h2>
+          <p className="eventStart">{event.created}</p>
+          <p className="event-location">{event.location}</p>
+
+          <h2>About event:</h2>
+          <p>
+            <a href="{event.htmlLink}">See Details on Google Calendar</a>
+          </p>
+          <p>{event.description}</p>
+        </div>
+      ) : null}
     </li>
   );
 };
