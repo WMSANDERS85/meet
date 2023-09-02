6c5e3e495d1d92e0f2d474f51d9551f06d4cf32c
Wayne Sanders <107742337+WMSANDERS85@users.noreply.github.com>
2023-09-01 23:19:05 -0700

    Refactor code to resolve linting errors

--- src/_test_/CitySearch.test.js
+++ src/_test_/CitySearch.test.js
@@ -1,77 +1,65 @@
-/* eslint-disable testing-library/no-render-in-setup */
-/* eslint-disable testing-library/prefer-screen-queries */
-import {render} from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import CitySearch from '../components/CitySearch';
 import {extractLocations, getEvents} from '../api';
 
 describe('<CitySearch /> component', () => {
-  let CitySearchComponent;
-  beforeEach(() => {
-    CitySearchComponent = render(<CitySearch />);
-  });
   test('renders text input', () => {
-    const cityTextBox = CitySearchComponent.queryByRole('textbox');
+    render(<CitySearch />);
+    const cityTextBox = screen.getByRole('textbox');
     expect(cityTextBox).toBeInTheDocument();
     expect(cityTextBox).toHaveClass('city');
   });
 
   test('suggestions list is hidden by default', () => {
-    const suggestionList = CitySearchComponent.queryByRole('list');
-    expect(suggestionList).not.toBeInTheDocument();
+    render(<CitySearch />);
+    expect(screen.queryByRole('list')).not.toBeInTheDocument();
   });
 
   test('renders a list of suggestions when city textbox gains focus', async () => {
-    const user = userEvent.setup();
-    const cityTextBox = CitySearchComponent.queryByRole('textbox');
-    await user.click(cityTextBox);
-    const suggestionList = CitySearchComponent.queryByRole('list');
-    expect(suggestionList).toBeInTheDocument();
-    expect(suggestionList).toHaveClass('suggestions');
+    render(<CitySearch />);
+    const cityTextBox = screen.getByRole('textbox');
+    await userEvent.click(cityTextBox);
+    expect(screen.getByRole('list')).toBeInTheDocument();
+    expect(screen.getByRole('list')).toHaveClass('suggestions');
   });
 
   test('updates list of suggestions correctly when user types in city textbox', async () => {
-    const user = userEvent.setup();
     const allEvents = await getEvents();
     const allLocations = extractLocations(allEvents);
-    CitySearchComponent.rerender(<CitySearch allLocations={allLocations} />);
+    render(<CitySearch allLocations={allLocations} />);
 
-    // user types "Berlin" in city textbox
-    const cityTextBox = CitySearchComponent.queryByRole('textbox');
-    await user.type(cityTextBox, 'Berlin');
+    const cityTextBox = screen.getByRole('textbox');
+    await userEvent.type(cityTextBox, 'Berlin');
 
-    // filter allLocations to locations matching "Berlin"
-    const suggestions = allLocations
-      ? allLocations.filter((location) => {
-          return (
-            location.toUpperCase().indexOf(cityTextBox.value.toUpperCase()) > -1
-          );
-        })
-      : [];
+    const suggestions = allLocations.filter((location) => {
+      return (
+        location.toUpperCase().indexOf(cityTextBox.value.toUpperCase()) > -1
+      );
+    });
 
-    // get all <li> elements inside the suggestion list
-    const suggestionListItems = CitySearchComponent.queryAllByRole('listitem');
+    const suggestionListItems = screen.queryAllByRole('listitem');
     expect(suggestionListItems).toHaveLength(suggestions.length + 1);
+
     for (let i = 0; i < suggestions.length; i += 1) {
       expect(suggestionListItems[i].textContent).toBe(suggestions[i]);
     }
   });
 
-  test('renders the suggestion text in the textbox upon clicking on the suggestion', async () => {
-    const user = userEvent.setup();
+  test('renders the suggestion text in the textbox upon clicking on the suggestion and hides suggestions', async () => {
     const allEvents = await getEvents();
     const allLocations = extractLocations(allEvents);
-    CitySearchComponent.rerender(<CitySearch allLocations={allLocations} />);
+    render(<CitySearch allLocations={allLocations} />);
 
-    const cityTextBox = CitySearchComponent.queryByRole('textbox');
-    await user.type(cityTextBox, 'Berlin');
+    const cityTextBox = screen.getByRole('textbox');
+    await userEvent.type(cityTextBox, 'Berlin');
 
-    // the suggestion's textContent look like this: "Berlin, Germany"
-    const BerlinGermanySuggestion =
-      CitySearchComponent.queryAllByRole('listitem')[0];
-
-    await user.click(BerlinGermanySuggestion);
+    const BerlinGermanySuggestion = screen.queryAllByRole('listitem')[0];
+    await userEvent.click(BerlinGermanySuggestion);
 
     expect(cityTextBox).toHaveValue(BerlinGermanySuggestion.textContent);
+
+    // Check if suggestions are hidden
+    expect(screen.queryByRole('list')).not.toBeInTheDocument();
   });
 });
