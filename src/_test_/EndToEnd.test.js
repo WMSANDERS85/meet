74605816311354e245200c4e8e13ffc0fa00b813
Wayne Sanders <107742337+WMSANDERS85@users.noreply.github.com>
2023-09-09 13:27:21 -0700

    Add end=to-end testing for Feature 1: Filter Events by City

--- src/_test_/EndToEnd.test.js
+++ src/_test_/EndToEnd.test.js
@@ -38,3 +38,50 @@ describe('show/hide detials', () => {
     expect(eventDetails).toBeNull();
   });
 });
+
+describe('Filter events by city', () => {
+  jest.setTimeout(30000);
+  let browser;
+  let page;
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 250,
+      ignoreDefaultArgs: ['--disable-extensions'],
+    }); // open browser
+    page = await browser.newPage(); // open new page
+    await page.goto('http://localhost:3000/'); // go to localhost:3000
+    await page.waitForSelector('.event'); // wait for event class
+  });
+
+  afterAll(() => {
+    browser.close(); // close browser
+  });
+
+  test('When user hasn’t searched for a city, show upcoming events from all cities.', async () => {
+    const events = await page.$('.event');
+    expect(events).toBeDefined();
+  });
+
+  test('User should see a list of suggestions when they search for a city.', async () => {
+    await page.type('.city', 'Berlin');
+    const suggestions = await page.$('.suggestions li');
+    expect(suggestions).toBeDefined();
+  });
+
+  test('User can select a city from the suggested list.', async () => {
+    await page.click('.suggestions li');
+    const events = await page.$('.event');
+    expect(events).toBeDefined();
+  });
+
+  test('All displayed events match the selected city.', async () => {
+    const eventLocations = await page.$$('.event .event-location');
+
+    for (const location of eventLocations) {
+      const text = await page.evaluate((el) => el.textContent, location);
+      expect(text).toBe('Berlin, Germany');
+    }
+  });
+});
