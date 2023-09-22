4dd9b4d80f91ca0a002e3d00eeb5566587c9f5af
Wayne Sanders <107742337+WMSANDERS85@users.noreply.github.com>
2023-09-21 22:23:08 -0700

    Add warning alert to let users know when they are offline

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
 import {extractLocations, getEvents} from './api';
-import {InfoAlert, ErrorAlert} from './components/Alert';
+import {InfoAlert, ErrorAlert, WarningAlert} from './components/Alert';
 import './App.css';
 
 const App = () => {
@@ -13,6 +13,7 @@ const App = () => {
   const [currentCity, setCurrentCity] = useState('See all cities');
   const [infoAlert, setInfoAlert] = useState('');
   const [errorAlert, setErrorAlert] = useState('');
+  const [warningAlert, setWarningAlert] = useState('');
 
   const fetchData = async () => {
     const allEvents = await getEvents();
@@ -25,6 +26,11 @@ const App = () => {
   };
 
   useEffect(() => {
+    if (navigator.online === false) {
+      setWarningAlert('You are offline. Events shown may not be up-to-date.');
+    } else {
+      setWarningAlert('');
+    }
     fetchData();
   }, [currentCity, currentNoe]);
 
@@ -37,6 +43,7 @@ const App = () => {
       <div className="alerts-container">
         {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
         {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
+        {warningAlert.length ? <WarningAlert text={warningAlert} /> : null}
       </div>
 
       <CitySearch
